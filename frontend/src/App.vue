7ef0398f5cc68a62e62e714259920478280a9661
jacq <jacq@guiley.me>
2022-10-02 19:30:40 -0700

    Got the site working better

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,32 +5,37 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header
-    class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-    <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-      <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-        <use xlink:href="#bootstrap"></use>
-      </svg>
-    </a>
+  <div>
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-light text-decoration-none">
+        <svg class="bi me-2" width="40" height="32">
+          <use xlink:href="/" />
+        </svg>
+        <span class="fs-4">Keyo Art</span>
+      </a>
 
-    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-      <li>
-        <RouterLink to="/" class="nav-link px-2 link-light">Home</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/team" class="nav-link px-2 link-light">Team</RouterLink>
-      </li>
-      <li><a href="#" class="nav-link px-2 link-light" disabled>FAQs</a></li>
-      <li><a href="#" class="nav-link px-2 link-light">Gallery</a></li>
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li>
+          <RouterLink to="/home" class="nav-link px-2 link-light">Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/team" class="nav-link px-2 link-light">Team</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/questions" class="nav-link px-2 link-light">Questions</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/gallery" class="nav-link px-2 link-light">Gallery</RouterLink>
+        </li>
+
+        <button type="button" class="btn btn-outline-primary me-2">Login</button>
+        <button type="button" class="btn btn-primary">Sign-up</button>
     </ul>
-
-    <div class="col-md-3 text-end">
-      <button type="button" class="btn btn-outline-primary me-2">Login</button>
-      <button type="button" class="btn btn-primary">Sign-up</button>
-    </div>
-  </header>
+    </header>
+  </div>
 
   <RouterView />
+
 </template>
 
 <style scoped>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,20 @@
+
+import GalleryPageVue from '@/views/GalleryPage.vue'
 import TeamPageVue from '@/views/TeamPage.vue'
+import QuestionsPageVue from '@/views/QuestionsPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomePage.vue'
+import OpenPageVue from '@/views/OpenPage.vue'
+import HomePageVue from '@/views/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: HomeView },
+    { path: '/', component: OpenPageVue },
+    { path: '/home', component: HomePageVue },
     { path: '/team', component: TeamPageVue },
-    
+    { path: '/questions', component: QuestionsPageVue },
+    { path: '/gallery', component: GalleryPageVue },
+
   ]
 })
 
