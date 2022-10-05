dce270d8bd187b74d5d91b90c5bc33c10061c9db
Andrea Mireles <andrea.mireles4601@gmail.com>
2022-10-04 23:06:41 -0500

    base of team page

--- frontend/src/views/TeamPage.vue
+++ frontend/src/views/TeamPage.vue
@@ -1,5 +1,26 @@
 <template>
-  <h1>Our Team</h1>
+<div class="container">
+  <h1><u>Our Team</u></h1>
+  <br>
+    <h5>Meet our team!</h5>
+    <p>
+        - Pierre Quereuil: 
+        <br>
+        - Nishant Perla: 
+        <br>
+        - Brent Kong: 
+        <br>
+        - Engoren Lim: 
+        <br>
+        - Jacq G: 
+        <br>
+        - Andrea Mireles: 
+        <br>
+    </p>
+    <h1><u>Ai Camp</u></h1>
+    <br>
+    <p> This is a camp of Ai! </p>
+    </div>
 </template>
 
 <style>
