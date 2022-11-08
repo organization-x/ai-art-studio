84f2f45e571fb1f81d2017cab8c6a91636f6efc4
jacq <jacq@guiley.me>
2022-11-07 20:25:17 -0800

    Added output stage

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,118 +1,139 @@
 <template>
   <div class="container-fluid mt-4">
-    <h2>Canvas</h2>
-    <div class="row">
-      <div class="col d-flex">
-        <div class="d-flex flex-column pe-2">
-          <!--Paint Brush-->
-          <button
-            @click="data.eraser = false"
-            class="btn-tool"
-            :class="{ active: !data.eraser }"
-          >
-            <i class="fa-solid fa-paintbrush"></i>
-          </button>
-          <!--erasser-->
-          <button
-            @click="data.eraser = true"
-            class="btn-tool"
-            :class="{ active: data.eraser }"
-          >
-            <i class="fa-solid fa-eraser"></i>
-          </button>
-          <select v-model="data.line" class="btn-tool">
-            <option v-for="n in 25" :key="'option-' + n" :value="n">
-              {{ n }}
-            </option>
-          </select>
+    <div v-if="data.status === 'drawing'">
+      <h2>Canvas</h2>
+      <div class="row">
+        <div class="col d-flex">
+          <div class="d-flex flex-column pe-2">
+            <!--Paint Brush-->
+            <button
+              @click="data.eraser = false"
+              class="btn-tool"
+              :class="{ active: !data.eraser }"
+            >
+              <i class="fa-solid fa-paintbrush"></i>
+            </button>
+            <!--erasser-->
+            <button
+              @click="data.eraser = true"
+              class="btn-tool"
+              :class="{ active: data.eraser }"
+            >
+              <i class="fa-solid fa-eraser"></i>
+            </button>
+            <select v-model="data.line" class="btn-tool">
+              <option v-for="n in 25" :key="'option-' + n" :value="n">
+                {{ n }}
+              </option>
+            </select>
 
-          <!-- Palete -->
-          <div class="hidden-palatte-parent">
-            <input
-              type="color"
-              v-model="data.color"
-              ref="inputColor"
-              class="hidden-palatte"
-            />
+            <!-- Palete -->
+            <div class="hidden-palatte-parent">
+              <input
+                type="color"
+                v-model="data.color"
+                ref="inputColor"
+                class="hidden-palatte"
+              />
+              <button
+                type="button"
+                @click.prevent="($refs as any).inputColor.click()"
+                class="btn-tool"
+                :style="{ color: data.color }"
+              >
+                <i class="fa-solid fa-palette"></i>
+              </button>
+            </div>
+
+            <div class="hidden-palatte-parent">
+              <input
+                type="color"
+                v-model="data.backgroundColor"
+                @change="($refs as any).VueCanvasDrawing?.redraw()"
+                ref="inputBackgroundColor"
+                class="hidden-palatte"
+              />
+              <button
+                type="button"
+                @click.prevent="($refs as any).inputBackgroundColor.click()"
+                class="btn-tool"
+                :style="{ color: data.backgroundColor }"
+              >
+                <i class="fa-solid fa-fill-drip"></i>
+              </button>
+            </div>
+
             <button
               type="button"
-              @click.prevent="($refs as any).inputColor.click()"
+              @click.prevent="($refs as any).VueCanvasDrawing.reset()"
               class="btn-tool"
-              :style="{ color: data.color }"
             >
-              <i class="fa-solid fa-palette"></i>
+              <i class="fa-solid fa-trash"></i>
             </button>
-          </div>
-
-          <div class="hidden-palatte-parent">
-            <input
-              type="color"
-              v-model="data.backgroundColor"
-              @change="($refs as any).VueCanvasDrawing?.redraw()"
-              ref="inputBackgroundColor"
-              class="hidden-palatte"
-            />
             <button
               type="button"
-              @click.prevent="($refs as any).inputBackgroundColor.click()"
+              @click.prevent="send()"
               class="btn-tool"
-              :style="{ color: data.backgroundColor }"
             >
-              <i class="fa-solid fa-fill-drip"></i>
+              <i class="fa-solid fa-paper-plane"></i>
+              <!-- fill background -->
             </button>
           </div>
+          <div class="flex-grow-1 position-relative">
+            <VueDrawingCanvas
+              ref="VueCanvasDrawing"
+              v-model:image="data.image"
+              :stroke-type="data.strokeType"
+              :line-cap="data.lineCap"
+              :line-join="data.lineJoin"
+              :fill-shape="data.fillShape"
+              :eraser="data.eraser"
+              :lineWidth="data.line"
+              :color="data.color"
+              :background-color="data.backgroundColor"
+              :lock="data.disabled"
+              :width="data.width"
+              :height="data.height"
+              class="rounded-1 border"
+            />
+            <Dragger
+              @started="onDragStart()"
+              @moved="onDrag($event)"
+              :style="{
+                position: 'absolute',
+                left: data.width + 'px',
+                top: data.height + 'px',
+              }"
+            />
 
-          <button
-            type="button"
-            @click.prevent="($refs as any).VueCanvasDrawing.reset()"
-            class="btn-tool"
-          >
-            <i class="fa-solid fa-trash"></i>
-            <!-- fill background -->
-          </button>
-        </div>
-        <div class="flex-grow-1 position-relative">
-          <VueDrawingCanvas
-            ref="VueCanvasDrawing"
-            v-model:image="data.image"
-            :stroke-type="data.strokeType"
-            :line-cap="data.lineCap"
-            :line-join="data.lineJoin"
-            :fill-shape="data.fillShape"
-            :eraser="data.eraser"
-            :lineWidth="data.line"
-            :color="data.color"
-            :background-color="data.backgroundColor"
-            :lock="data.disabled"
-            :width="data.width"
-            :height="data.height"
-            class="rounded-1 border"
-          />
-          <Dragger
-            @started="onDragStart()"
-            @moved="onDrag($event)"
-            :style="{
-              position: 'absolute',
-              left: data.width + 'px',
-              top: data.height + 'px',
-            }"
-          />
-
-          <h4>Text to Image</h4>
-          <input
-            class="form-control rounded-1"
-            type="text"
-            placeholder="Cartoon, Relistic, etc . . ."
-            aria-label="default input example"
-          />
+            <h4>Text to Image</h4>
+            <input
+              class="form-control rounded-1"
+              type="text"
+              placeholder="Cartoon, Relistic, etc . . ."
+              aria-label="default input example"
+            />
+          </div>
         </div>
       </div>
-      <div class="col-12"></div>
     </div>
-    <div class="row">
-      <div class="col-12">
-        <h3>Output:</h3>
-        <img :src="data.image" class="rounded-1" />
+
+    <div v-if="data.status === 'sending'">
+      <div class="row">
+        <div class="col-12">Keyo is Thinking -_-</div>
+      </div>
+    </div>
+
+    <div v-if="data.status === 'success'">
+      <div class="row">
+        <div class="col-12">
+          <h3>AI Generated Result:
+            <button @click="data.status ='drawing'" class = "btn btn-secondary">
+            DRAW AGAIN
+          </button>
+          </h3>
+          <img :src="data.imageResponse" class="rounded-1" />
+        </div>
       </div>
     </div>
   </div>
@@ -123,9 +144,10 @@ import VueDrawingCanvas from "vue-drawing-canvas";
 import { reactive, ref } from "vue";
 import Dragger from "@/components/Dragger.vue";
 
-
 const data = reactive({
+  status: "drawing" as "drawing" | "sending" | "success",
   image: "",
+  imageResponse: "",
   eraser: false,
   disabled: false,
   fillShape: false,
@@ -141,20 +163,27 @@ const data = reactive({
   height: 400,
   width: 800,
 });
-const VueCanvasDrawing = ref<any>(null)
-let startX = 0
-let startY = 0
+const VueCanvasDrawing = ref<any>(null);
+let startX = 0;
+let startY = 0;
 
 function onDragStart() {
-  startX = data.width
-  startY = data.height
+  startX = data.width;
+  startY = data.height;
 }
 
 function onDrag($event) {
   // console.log("onDrag",$event)
-  data.width = startX + $event.x
-  data.height = startY + $event.y
-  window.setTimeout(() => VueCanvasDrawing.value.redraw())
+  data.width = startX + $event.x;
+  data.height = startY + $event.y;
+  window.setTimeout(() => VueCanvasDrawing.value.redraw());
+}
+
+async function send() {
+  data.status = "sending"
+  data.imageResponse = await data.image 
+
+  data.status = "success"
 }
 </script>
   
