a30641f750c39ad1d386ac7d98ce13ca4e8726a5
Andrea Mireles <andrea.mireles4601@gmail.com>
2022-11-10 17:18:45 -0600

    Updated Team Page

--- frontend/src/views/TeamPage.vue
+++ frontend/src/views/TeamPage.vue
@@ -1,11 +1,69 @@
 <style>
 .dot {
-  height: 200px;
-  width: 200px;
-  background-color: rgb(241, 8, 90);
-  border-radius: 50%;
+  height: 225px;
+  width: 225px;
+  background-color: rgba(244, 122, 165, 0.779);
+  border-radius: 100%;
   display: inline-block;
 }
+
+.dot1 {
+  height: 225px;
+  width: 225px;
+  background-color: rgba(222, 101, 143, 0.814);
+  border-radius: 100%;
+  display: inline-block;
+}
+
+.dot2 {
+  height: 225px;
+  width: 225px;
+  background-color: rgba(235, 93, 142, 0.814);
+  border-radius: 100%;
+  display: inline-block;
+}
+
+.dot3 {
+  height: 225px;
+  width: 225px;
+  background-color: rgba(228, 63, 120, 0.814);
+  border-radius: 100%;
+  display: inline-block;
+}
+
+.dot4 {
+  height: 225px;
+  width: 225px;
+  background-color: rgba(224, 44, 107, 0.814);
+  border-radius: 100%;
+  display: inline-block;
+}
+
+.dot5 {
+  height: 225px;
+  width: 225px;
+  background-color: rgba(228, 0, 80, 0.814);
+  border-radius: 100%;
+  display: inline-block;
+}
+
+.dot6 {
+  height: 125px;
+  width: 300px;
+  background-color: rgba(0, 0, 0, 0.882);
+  border-radius: 10%;
+  display: inline-block;
+}
+
+.footer {
+   position: relative;
+   left: 0;
+   bottom: 100;
+   width: 100%;
+   height: 10000%;
+   background-color: black;
+}
+
 </style>
 
 <template>
@@ -17,86 +75,97 @@
     <br/>
     <br/>
 
-    <div><span class="dot"><p style="text-align:center;">
-      <br>
-      <br>
-      <br>
-      1.) Pierre Quereuil: Team leader!
+    <div style="text-align:left"><span class="dot">
+        <br>
+      <h1 style="text-align:center"><b>1</b></h1>
+      <p style="text-align:center;font-style:italic"><u>
+      Pierre Quereuil:</u></p>
+      <p style="text-align:center"> The team leader that led us through the project's journey!
       </p>
-      </span>
-  </div>
-
-<br>
-<br>
-
-    <div><span class="dot"><p style="text-align:center;">
-      <br>
-      <br>
-      <br>
-      2.) Nishant Perla: Product manager!</p>
   </span>
   </div>
 
-      <br>
-      <br>
 
-      <div><span class="dot"><p style="text-align:center;">
+    <div style="text-align:center"><span class="dot1">
       <br>
-      <br>
-      <br>
-      3.) Brent Kong: Backend developer!</p>
+      <h1 style="text-align:center"><b>2</b></h1>
+      <p style="text-align:center;font-style:italic"><u>
+      Nishant Perla:</u></p>
+      <p style="text-align:center"> The product manager that aided the backend with coding!
+      </p>
   </span>
   </div>
 
-      <br>
-      <br>
 
-      <div><span class="dot"><p style="text-align:center;">
-      <br>
-      <br>
-      <br>
-      4.) Engoron Lim: Backend developer!</p>
+      <div style="text-align:right"><span class="dot2">
+        <br>
+      <h1 style="text-align:center"><b>3</b></h1>
+      <p style="text-align:center;font-style:italic"><u>
+      Engoron Lim:</u></p>
+      <p style="text-align:center"> Discovered Django and coded the Stable Diffusion Model!
+      </p>
   </span>
   </div>
 
-      <br>
-      <br>
 
-      <div><span class="dot"><p style="text-align:center;">
-      <br>
-      <br>
-      <br>
-      5.) Jacq G: Frontend developer!</p>
+      <div style="text-align:center"><span class="dot3">
+        <br>
+      <h1 style="text-align:center"><b>4</b></h1>
+      <p style="text-align:center;font-style:italic"><u>
+      Brent Kong:</u></p>
+      <p style="text-align:center"> Helped in the coding with the Stable Diffusion Model!
+      </p>
   </span>
   </div>
 
-      <br>
-      <br>
+      <div style="text-align:left"><span class="dot4">
+        <br>
+      <h1 style="text-align:center"><b>5</b></h1>
+      <p style="text-align:center;font-style:italic"><u>
+      Jacq G:</u></p>
+      <p style="text-align:center"> Designer of the Keyo-Art, Home, and Questions pages!
+      </p>
+  </span>
+  </div>
 
-      <div><span class="dot"><p style="text-align:center;">
-      <br>
-      <br>
-      <br>
-      6.) Andrea Mireles: Frontend developer!</p>
+      <div style="text-align:center"><span class="dot5">
+        <br>
+      <h1 style="text-align:center"><b>6</b></h1>
+      <p style="text-align:center;font-style:italic"><u>
+      Andrea Mireles:</u></p>
+      <p style="text-align:center"> Designer of the Team Page!
+      </p>
   </span>
   </div>
     </p>
 </div>
 
 <br>
-<br>
-<br>
-<br>
 
     <div class="container"><h1 style="text-align:right;font-family:cursive;font-style:italic;font-weight:900;font-variant:small-caps"><b><u>Ai Camp</u></b></h1>
     <br/>
-    <br/>
-    <p style="text-align:right;">
-    Ai camp is an online community that shares ideas and dreams of computer science. 
-<br>
-<a href="https://www.ai-camp.org/">Ai Camp!</a>
+    <p style="text-align: right;">
+    Ai camp is an online community that shares ideas and create projects with <br>
+    artificial intelligence! Every year AiCamp hosts several programs for students whose passion is <br>
+    computer science to develop skills in coding, teamwork, organization, and real world problem-solving!
     </p>
   </div>
+
+  <br>
+
+  <div style="text-align:center"><span class="dot6">
+    <br>
+    <br>
+    <a style="text-align:center;font-weight:1000;font-style:bold;color:violet" href="https://www.ai-camp.org/"><u>The AiCamp Website!</u></a>
+  </span>
+  </div>
+
+  <br> 
+<br>
+
+  <div class="footer">
+    <br>
+</div>
 
   </body>
 </template>
